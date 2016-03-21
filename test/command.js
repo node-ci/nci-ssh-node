f2a07d304531e23dd70f764f99c4abf353ce69c5
oleg <oleg.korobenko@gmail.com>
2016-03-21 20:40:56 +0300

    add more tests for command

--- test/command.js
+++ test/command.js
@@ -9,11 +9,133 @@ describe('ssh shell command', function() {
 	function SpawnCommand() {
 	}
 
-	SpawnCommand.prototype.setParams = function() {};
+	var app = {lib: {command: {}}};
 
-	var app = {lib: {command: {SpawnCommand: SpawnCommand}}};
+	describe('constructor', function() {
+		var Command, parentConstructorSpy;
+
+		beforeEach(function() {
+			app.lib.command.SpawnCommand = sinon.spy();
+			parentConstructorSpy = app.lib.command.SpawnCommand;
+
+			Command = getCommandConstructor(app);
+		});
+
+		after(function() {
+			delete app.lib.command.SpawnCommand;
+		});
+
+		it('should call parent constructor', function() {
+			var command = new Command({});
+			expect(parentConstructorSpy.calledOnce).equal(true);
+		});
+
+		it('should call parent constructor with params', function() {
+			var params = {collectOut: true},
+				command = new Command(params);
+
+			expect(parentConstructorSpy.getCall(0).args[0]).eql(params);
+		});
+
+		it('should set shell by default to /bin/sh', function() {
+			var command = new Command({});
+			expect(command.shell).equal('/bin/sh');
+		});
+
+		it('should allow set another shell', function() {
+			var command = new Command({shell: '/bin/bash'});
+			expect(command.shell).equal('/bin/bash');
+		});
+	});
+
+	describe('set params', function() {
+		before(function() {
+			app.lib.command.SpawnCommand = SpawnCommand;
+		});
+
+		var Command, parentSetParamsSpy;
+
+		beforeEach(function() {
+			SpawnCommand.prototype.setParams = sinon.spy(function(params) {
+				if (params.cwd) this.cwd = params.cwd;
+			});
+			parentSetParamsSpy = SpawnCommand.prototype.setParams;
+
+			Command = getCommandConstructor(app);
+		});
+
+		after(function() {
+			delete app.lib.command.SpawnCommand;
+			delete SpawnCommand.prototype.setParams;
+		});
+
+		it('should call parent set params', function() {
+			var command = new Command({});
+			command.setParams({});
+			expect(parentSetParamsSpy.calledOnce).equal(true);
+		});
+
+		it('should call parent set params with params', function() {
+			var params = {opt: '1'},
+				command = new Command({});
+
+			command.setParams(params);
+			expect(parentSetParamsSpy.getCall(0).args[0]).eql(params);
+		});
+
+		it('should rename cwd to _cwd', function() {
+			var command = new Command({});
+			command.setParams({cwd: '/tmp'});
+			expect(command).not.have.key('cwd');
+			expect(command._cwd).equal('/tmp');
+		});
+
+		it('should allow set host', function() {
+			var command = new Command({});
+			command.setParams({host: '192.168.0.1'});
+			expect(command.host).equal('192.168.0.1');
+		});
+
+		it('should allow set port', function() {
+			var command = new Command({});
+			command.setParams({port: 1122});
+			expect(command.port).equal(1122);
+		});
+
+		it('should allow set user', function() {
+			var command = new Command({});
+			command.setParams({user: 'ci'});
+			expect(command.user).equal('ci');
+		});
+
+		it('should allow set identity file', function() {
+			var command = new Command({});
+			command.setParams({identityFile: '~/.id_rsa_01'});
+			expect(command.identityFile).equal('~/.id_rsa_01');
+		});
+
+		it('should allow set additional args', function() {
+			var command = new Command({});
+			command.setParams({args: ['-Y', '-q']});
+			expect(command.args).eql(['-Y', '-q']);
+		});
+
+		it('should not allow set arbitrary option', function() {
+			var command = new Command({});
+			command.setParams({someOption: '123'});
+			expect(command).not.have.key('someOption');
+		});
+	});
 
 	describe('run method', function() {
+		before(function() {
+			app.lib.command.SpawnCommand = SpawnCommand;
+		});
+
+		after(function() {
+			delete app.lib.command.SpawnCommand;
+		});
+
 		var Command, runSpy;
 
 		beforeEach(function() {
@@ -53,9 +175,7 @@ describe('ssh shell command', function() {
 
 		it('should always add and force identity file to command', function() {
 			var command = new Command({});
-			command.setParams({
-				identityFile: '~/.ssh/id_rsa_01'
-			});
+			command.identityFile = '~/.ssh/id_rsa_01';
 			command.run({cmd: 'beep', args: ['1', '2']});
 
 			var params = runSpy.getCall(0).args[0];
@@ -75,9 +195,7 @@ describe('ssh shell command', function() {
 
 		it('should optionally add port to command', function() {
 			var command = new Command({});
-			command.setParams({
-				port: 1122
-			});
+			command.port = 1122;
 			command.run({cmd: 'beep', args: ['1', '2']});
 
 			var params = runSpy.getCall(0).args[0];
@@ -87,9 +205,7 @@ describe('ssh shell command', function() {
 
 		it('should optionally add arbitrary args to command', function() {
 			var command = new Command({});
-			command.setParams({
-				args: ['-Y', '-q']
-			});
+			command.args = ['-Y', '-q'];
 			command.run({cmd: 'beep', args: ['1', '2']});
 
 			var params = runSpy.getCall(0).args[0];
@@ -99,10 +215,8 @@ describe('ssh shell command', function() {
 
 		it('should always add user and host to command', function() {
 			var command = new Command({});
-			command.setParams({
-				host: '192.168.0.1',
-				user: 'nci'
-			});
+			command.host = '192.168.0.1';
+			command.user = 'nci';
 			command.run({cmd: 'beep', args: ['1', '2']});
 
 			var params = runSpy.getCall(0).args[0];
@@ -116,26 +230,6 @@ describe('ssh shell command', function() {
 			var params = runSpy.getCall(0).args[0],
 				cmd = params.args[params.args.length - 1];
 			expect(cmd).eql('/bin/sh -c \'beep "1" "2"\'');
-		});
-
-		it('should set /bin/sh as default shell', function() {
-			var command = new Command({});
-			command.run({cmd: 'beep', args: ['1', '2']});
-
-			var params = runSpy.getCall(0).args[0],
-				cmd = params.args[params.args.length - 1];
-			expect(cmd).match(new RegExp('^/bin/sh '));
-		});
-
-		it('should allow to pass another shell', function() {
-			var command = new Command({
-				shell: '/bin/bash'
-			});
-			command.run({cmd: 'beep', args: ['1', '2']});
-
-			var params = runSpy.getCall(0).args[0],
-				cmd = params.args[params.args.length - 1];
-			expect(cmd).match(new RegExp('^/bin/bash '));
 		});
 
 		it('should pass options cwd as cd to command', function() {
@@ -188,11 +282,9 @@ describe('ssh shell command', function() {
 
 		it('should work in general', function() {
 			var command = new Command({});
-			command.setParams({
-				host: '192.168.0.1',
-				user: 'nci',
-				identityFile: '~/.ssh/id_rsa_01'
-			});
+			command.host = '192.168.0.1';
+			command.user = 'nci';
+			command.identityFile = '~/.ssh/id_rsa_01';
 			command.run({cmd: 'beep', args: ['1', '2']});
 
 			var params = runSpy.getCall(0).args[0];
